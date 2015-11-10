c33090d6e4c2cb8c3cbdff154a373d970b3332c7
John L <john.m.lenehan@gmail.com>
2015-11-10 15:14:01 -0500

    default rendering enging set to swig working properly

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var swig = require('swig')
 
 var port = 3000;
 
+//take out this line after completion for better performance
+swig.setDefaults({ cache: false });
+
+app.engine('html', swig.renderFile);
+app.set('view engine', "html" );
+app.set('views', __dirname + "/views");
 app.use(function(req,res,next){
 	console.log("Request verb: " + req.method)
 	console.log("Response Status Code: "+ res.statusCode)
@@ -10,13 +17,18 @@ app.use(function(req,res,next){
 });
 
 app.get("/",function(req,res){
-	res.send("This is the home site");
-	//console.log("this is the home site");
+	var testObj = {
+
+	title: "An Example",
+	people: [{age: 54, name: "Gandalf"},{name: "Frodo"},{name: "Hermione"}]
+	}
+	res.render("index", testObj, function(err,output){
+			if(err) throw err;
+			res.send(output);
+		});
 });
-
-
-
 
 app.listen(port,function(){
 	console.log("Listening on port " + port);
 });
+
